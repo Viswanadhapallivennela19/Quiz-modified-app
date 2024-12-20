c3d0e1fd0eba62b4a1daeaa5372dcc59ce5bf63d
Viswanadhapallivennela19 <165765409+Viswanadhapallivennela19@users.noreply.github.com>
2024-12-20 23:02:22 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
- 
+ document.addEventListener('contextmenu', function(event) {
+    event.preventDefault();
+    alert("Right-click is disabled on this page.");
+});
+document.onkeydown = function(e) {
+    if (e.keyCode == 123) { // F12 key
+        return false;
+    }
+    if (e.ctrlKey && e.shiftKey && e.keyCode == 'I'.charCodeAt(0)) { // Ctrl+Shift+I
+        return false;
+    }
+};
